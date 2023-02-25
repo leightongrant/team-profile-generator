96f060a71c55d1a66994062c15f8b130db4c0e66
Leighton Grant <oneal.grant@outlook.com>
2023-02-25 09:47:06 +0000

    Adds fs-extra module

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const inquirer = require("inquirer");
+import { resolve, join } from 'path';
+import fs from 'fs-extra';
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = resolve('', 'output');
+const outputPath = join(OUTPUT_DIR, 'team.html');
 
-const render = require("./src/page-template.js");
-
+fs.outputFile(outputPath, '<h1>Hello World</h1>', (err) => {
+    if (err) console.log(err);
+});
+//console.log(typeof outputPath);
+// const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
