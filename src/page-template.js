3a1fa65537949504b87411af701df97d2e16a045
Leighton Grant <oneal.grant@outlook.com>
2023-02-28 12:16:36 +0000

    Adds styling

--- src/page-template.js
+++ src/page-template.js
@@ -11,14 +11,13 @@ const generateTeam = (team) => {
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item border-man bg-light bg-gradient text-dark opacity-75">ID: ${manager.getId()}
+                            <li class="list-group-item border-man bg-light bg-gradient text-dark opacity-75">ID:    ${manager.getId()}
                                 </li>
                             <li class="list-group-item border-man bg-light bg-gradient text-dark opacity-75">
-                                Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+                            <i class="fa-solid fa-envelope"></i> <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
                             </li>
                             <li class="list-group-item border-man bg-light bg-gradient text-dark opacity-75">
-                                Office
-                                Number: ${manager.getOfficeNumber()}</li>
+                            <i class="fa-solid fa-building"></i>    ${manager.getOfficeNumber()}</li>
                     </ul>
                 </div>
             </div>
@@ -38,11 +37,11 @@ const generateTeam = (team) => {
                 <div class="card-body">
                     <ul class="list-group">
                         <li class="list-group-item border-eng bg-light bg-gradient text-dark opacity-75">ID:
-                        ${engineer.getId()}</li>
+                           ${engineer.getId()}</li>
                         <li class="list-group-item border-eng bg-light bg-gradient text-dark opacity-75">
-                            Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+                        <i class="fa-solid fa-envelope"></i> <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
                         </li>
-                        <li class="list-group-item border-eng bg-light bg-gradient text-dark opacity-75">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                        <li class="list-group-item border-eng bg-light bg-gradient text-dark opacity-75"><i class="fa-brands fa-github"></i> <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
                     </ul>
                 </div>
             </div>
@@ -62,11 +61,11 @@ const generateTeam = (team) => {
                 <div class="card-body">
                     <ul class="list-group">
                         <li class="list-group-item border-int bg-light bg-gradient text-dark opacity-75">ID:
-                        ${intern.getId()}</li>
+                           ${intern.getId()}</li>
                         <li class="list-group-item border-int bg-light bg-gradient text-dark opacity-75">
-                            Email: <a href="${intern.getEmail()}">${intern.getEmail()}</a>
+                        <i class="fa-solid fa-envelope"></i> <a href="${intern.getEmail()}">${intern.getEmail()}</a>
                         </li>
-                        <li class="list-group-item list-group-item border-int bg-light bg-gradient text-dark opacity-75">School: ${intern.getSchool()}</li>
+                        <li class="list-group-item list-group-item border-int bg-light bg-gradient text-dark opacity-75"><i class="fa-solid fa-school"></i> ${intern.getSchool()}</li>
                     </ul>
                 </div>
             </div>
@@ -108,35 +107,32 @@ const render = (team) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta http-equiv="X-UA-Compatible" content="ie=edge" />
             <title>My Team</title>
-            <!-- new -->
+            
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap" rel="stylesheet">
-            <!-- new -->
-            <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
             <link rel="stylesheet" href="style.css">
             <script src="https://kit.fontawesome.com/c502137733.js"></script>
+            <script src="https://kit.fontawesome.com/b547bfc4bb.js" crossorigin="anonymous"></script>
         </head>
         
         <body>
-            <header class="container-fluid bg-dark bg-gradient">
+            <header class="container-fluid bg-gradient">
                 <div class="row">
-                    <div class="col jumbotron py-5 team-heading">
-                        <h1 class="text-center fc-clr5">My Team</h1>
+                    <div class="col jumbotron py-4 team-heading">
+                    <i class="fa-solid fa-users-line"></i>  <h1 class="text-center"> My Team</h1>
                     </div>
                 </div>
             </header>
-            <main class="bg-black">
+            <main class="">
                 <section class="container team-area py-5">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-2 g-lg-3 team-area">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 team-area">
                     ${generateTeam(team)}
                     </div>
                     </section>
                 </main>
-                <footer class="bg-dark bg-gradient">
-                    <p class="text-center text-light">© 2023</p>
+                <footer class="bg-gradient py-1">
+                    <p class="text-center">© 2023</p>
                 </footer>
             
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
